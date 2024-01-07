1256883b602cf5676aab6d5122b0d2ee63754809
angadk4 <kheraangad@gmail.com>
2024-01-06 20:00:29 -0500

    First steps

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+import scraper
+import createmodel
+import time
 
+SCROLL_RANGE = 3
 def main():
-    print("Running Script")
+    sc = scraper.Scraper()
+    sc.load_url()
+    print("Loaded KJIJI AUTOS...")
+    for i in range(SCROLL_RANGE):
+        time.sleep(2)
+        sc.scroll_down()
+        print("Scroll " + str(i+1))
+    print("Done scrolling, waiting for load...")
+    time.sleep(5)
+    print("Reading data now...")
+    sc.read_data()
+    print(sc.get_hrefs())
+    print(sc.get_features("https://www.kijijiautos.ca/" + str(sc.hrefs[0])))
+    print("\n\nFinished Running Script")
+    time.sleep(300)
 
 if __name__ == "__main__":
     main()
--- scraper.py
+++ scraper.py
@@ -1,4 +1,51 @@
-
+import time
+from bs4 import BeautifulSoup as bs
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 class Scraper:
     def __init__(self):
-        pass
+        chrome_options = Options()
+        # Using WebDriver Manager to handle the driver binaries
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+        self.url = "http://www.kijijiautos.ca/"
+        self.hrefs= []
+        self.features = []
+
+    def get_features(self, url):
+        self.get_info(url)
+        return self.features
+    def get_hrefs(self):
+        return self.hrefs
+    def load_url(self):
+        self.driver.get(self.url + 'cars/sedan')
+
+    def scroll_down(self):
+        self.driver.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+
+    def read_data(self):
+        main = self.driver.find_element(By.XPATH, '//*[@id="root"]/div[3]/div/section[5]/div/div/div[2]')
+        soup = bs(main.get_attribute("innerHTML"), "html.parser")
+        print(len(soup.findAll('a',{'class':'bcNN7t'})))
+        for elem in soup.findAll('a',{'class':'bcNN7t'}):
+            #print(elem.div['data-test-ad-id'])
+            self.hrefs.append('vip/{}'.format(str(elem.div['data-test-ad-id'])))
+
+    def get_info(self, url):
+        self.driver.get(url)
+        time.sleep(1)
+        cf = self.driver.find_element(By.XPATH, '//*[@id="tabpanel-features"]/div/aside/div[1]/div/div[1]/div[2]/ul')
+        soup = bs(cf.get_attribute("innerHTML"), "html.parser")
+        for li in soup.find_all('li'):
+            txt = li.text
+            if 'Make' in txt:
+                ma = (str(txt)[5:].strip())
+            if 'Model' in txt:
+                mo = (str(txt)[6:].strip())
+            if 'Year' in txt:
+                ye = (str(txt)[5:].strip())
+        self.features.append([ma, mo, ye])
+
+
